ef0d5248ede9db9bc112d0fbab232a288fa09140
Pavel Larin <www.larinpavel@mail.ru>
2023-11-02 01:58:23 +0500

    Initial commit

---
+++ src/main/java/com/example/Springboot/lar/SpringBootLarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.lar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springboot/lar/SpringBootLarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.lar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
