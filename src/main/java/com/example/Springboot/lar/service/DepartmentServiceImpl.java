3de65c5d4eab150d2c16755b8c667e1a0d3f5b31
Pavel Larin <www.larinpavel@mail.ru>
2023-11-03 14:33:31 +0500

    @PutMapping

--- src/main/java/com/example/Springboot/lar/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/Springboot/lar/service/DepartmentServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
@@ -33,6 +34,29 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public void deleteDepartmentById(Long departmentId) {
         departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartment(Long departmentId, Department department) {
+        Department depDB = departmentRepository.findById(departmentId).get();
+
+        if(Objects.nonNull(department.getDepartmentName()) &&
+                !"".equalsIgnoreCase(department.getDepartmentName())) {
+            depDB.setDepartmentName(department.getDepartmentName());
+        }
+
+        if(Objects.nonNull(department.getDepartmentCode()) &&
+                !"".equalsIgnoreCase(department.getDepartmentCode())) {
+            depDB.setDepartmentCode(department.getDepartmentCode());
+        }
+
+        if(Objects.nonNull(department.getDepartmentAddress()) &&
+                !"".equalsIgnoreCase(department.getDepartmentAddress())) {
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
+        }
+
+        return departmentRepository.save(depDB);
+
     }
 
 }
