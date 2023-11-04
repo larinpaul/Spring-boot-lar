7f10df01a010e2f886c0646fa6154a90d7aa68fd
Pavel Larin <www.larinpavel@mail.ru>
2023-11-05 00:45:05 +0500

    Added Lombok @Data (@Getters, @Setters, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode), @NoArgsConstructor, @AllArgsConstructor, @Builder

--- src/main/java/com/example/Springboot/lar/entity/Department.java
+++ src/main/java/com/example/Springboot/lar/entity/Department.java
@@ -1,5 +1,9 @@
 package com.example.Springboot.lar.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
@@ -11,6 +15,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
+@Data // Equivalent to @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
+@NoArgsConstructor // Default constructor
+@AllArgsConstructor // Constructor with all arguments
+@Builder // An entire Builder pattern will be implemented for this department service
 public class Department {
 
     @Id
@@ -22,55 +30,4 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
